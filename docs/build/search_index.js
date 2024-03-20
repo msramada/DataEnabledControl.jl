d84249fc775a5c81ba907923f16462a5f0ffe278
Mohammad <msramada@eng.ucsd.edu>
2024-03-20 13:24:13 -0500

    minor issues

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DataEnabledControl.jl","page":"DataEnabledControl.jl","title":"DataEnabledControl.jl","text":"","category":"section"},{"location":"","page":"DataEnabledControl.jl","title":"DataEnabledControl.jl","text":"Documentation for DataEnabledControl.jl","category":"page"}]
+}
